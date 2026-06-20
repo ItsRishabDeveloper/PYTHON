d2622344d4a5d61e174d80b6e19e2242ab73f713
RISHAB R K <itsrishabdeveloper@gmail.com>
2026-06-21 00:14:47 +0530

    Added Hello.py and Addition.py

---
+++ Addition.py
@@ -0,0 +1,9 @@
+print(5+6)
+a = 10
+b = 30
+print(a+b)
+def Add(q,w):
+    return q+w
+q = int(input("Enter first number: "))
+w = int(input("Enter second number: "))
+print("The sum of two numbers is: ", Add(q,w))

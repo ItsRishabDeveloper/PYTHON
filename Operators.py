f28a48d487664f4f1da96f74b1d62dc2ed463ca6
Rishab Developer <itsrishabdeveloper@gmail.com>
2026-06-24 01:07:51 +0530

    Add files via upload

---
+++ Operators.py
@@ -0,0 +1,41 @@
+a = 10
+b = 20
+c = 30
+d = 40
+e = 50
+f = 60
+#   Arithmetic operators
+print("Addition of a and b is", a + b)
+print("Subtraction of a and b is", a - b)
+print("Multiplication of a and b is", a * b)
+print("Division of a and b is", a / b)
+print("Modulus of a and b is", a % b)
+print("Exponent of a and b is", a ** b)
+# Assingment operators
+a+=5
+b+=5
+a*=2
+print("Value of a is", a)
+print("Value of b is", b)
+# Relational operators
+print(10==10)
+print(10!=10)
+print(10>10)
+print(10<10)
+print(10>=10)
+print(10<=10)
+# Logical operators
+print(10>5 and 10<20)
+print(10>5 or 10<20)
+print(not(10>5 and 10<20))
+# post Increment
+c = 10
+result = c
+c += 1
+print(result) 
+# Pre Increment
+c = 10
+c += 1
+result = c
+print(result)  
+print(c)       

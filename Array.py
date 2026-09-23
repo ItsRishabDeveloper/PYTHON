035cad02957debd95e27c23e639a394a80cdc7b3
Rishab Developer <itsrishabdeveloper@gmail.com>
2026-09-23 23:51:11 +0530

    Add files via upload

--- Array.py
+++ Array.py
@@ -1,5 +1,2 @@
-import numpy as np
-Matrix1_d = np.array([1, 2, 3, 4, 5])
-Matrix2_d = np.array([[1, 2, 3], [4, 5, 6]])
-Updated_Price = Matrix1_d * 2
-print(Updated_Price)
+arr = [10, 20, 30, 40, 50]
+print(arr[3])
